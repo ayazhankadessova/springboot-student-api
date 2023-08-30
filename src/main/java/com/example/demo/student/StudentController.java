06ff81423706ca40c54d5eb4143fa887735b7cea
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-08-30 14:41:28 +0600

    add: bootstrap PUT method

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,4 +39,16 @@ public class StudentController {
     public void deleteStudent(@PathVariable("studentId") Long id) {
         studentService.deleteStudent(id);
     }
+
+    @PutMapping(path = "{studentId}")
+    public void updateUser(
+            @PathVariable("studentId") String id,
+            @RequestParam(value = "userName", required = false) String userName,
+            @RequestParam(value = "email", required = false) String email) {
+        // Inside this method, you can update the user record based on the provided
+        // parameters
+        // Check if 'userName' or 'email' is not null and update the corresponding
+        // field(s) in the user record
+    }
+
 }
